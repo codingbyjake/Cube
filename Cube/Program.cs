31c6daf4e2b925c90a2eaf01fb25c0ff45685e45
Jacob Witte <wittejg@gmail.com>
2023-02-14 10:24:35 -0500

    Add project files.

---
+++ Cube/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Cube
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 1;
+            double cube = 0;
+            while(num <= 25)
+            {
+                if (num % 2 != 0)
+                {
+                    cube = Math.Pow(num, 3);
+                    //cube = num*num*num;
+                    Console.WriteLine($"The number {num} cubed is {cube}");
+                }
+                num++;
+            }
+        }
+    }
+}
